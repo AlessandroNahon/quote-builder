1d95f3dfe1a977bf15e3e70cb89dee8cdc6ed72b
alessandronahon <alessandronahon@gmail.com>
2023-10-05 19:43:10 -0400

    empty quote

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -7,23 +7,26 @@ import { LineItemDataInterface } from '../types'
 export default function Quote(): ReactElement {
   const { quote } = useContext(AppContext)
 
+
   return (
     <Browser>
       <h2>Quote</h2>
-      {quote.lineItems?.map((item: LineItemDataInterface) => (
-        <LineItem item={item} />
-      ))}
-      <div id="total">
-        <div className='spacer'></div>
-        <p> Subtotal: ${quote.subtotal}</p>
-        <label>Discounts</label>
-        <input type='number' />
-        <label>Tax</label>
-        <input type='number' />
-        <div className='spacer'></div>
-        <p>Total: ${quote.total}</p>
-      </div>
-
+      {quote.lineItems.length === 0 && <h3>Select a product.</h3>}
+      {quote.lineItems.length > 0 && <>
+        {quote.lineItems?.map((item: LineItemDataInterface) => (
+          <LineItem item={item} />
+        ))}
+        <div id="total">
+          <div className='spacer'></div>
+          <p> Subtotal: ${quote.subtotal}</p>
+          <label>Discounts</label>
+          <input type='number' />
+          <label>Tax</label>
+          <input type='number' />
+          <div className='spacer'></div>
+          <p>Total: ${quote.total}</p>
+        </div>
+      </>}
     </Browser>
   )
 }
